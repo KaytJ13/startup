6e20ae67b33d7a9dc543e0e5a2989c8942830cc9
Kate Norcross <130419451+KaytJ13@users.noreply.github.com>
2025-10-06 14:53:51 -0600

    Created app component files

---
+++ startup/src/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  return <div className="body">App will display here</div>;
+  // Deleted from body bg-dark text-light
+}
