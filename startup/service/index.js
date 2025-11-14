d5bb8fd9f7852f0fdc3cc34c28f4d8cbf982e34f
Kate Norcross <130419451+KaytJ13@users.noreply.github.com>
2025-11-14 11:43:42 -0700

    Added backend webSocket functionality

--- startup/service/index.js
+++ startup/service/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const uuid = require('uuid');
 const app = express();
 const DB = require('./database.js');
+const { peerProxy } = require('./peerProxy.js');
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
@@ -142,3 +143,5 @@ function setAuthCookie(res, authToken) {
 const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+peerProxy(httpService);
